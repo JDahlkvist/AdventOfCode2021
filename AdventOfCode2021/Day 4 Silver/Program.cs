1e29e64845f870b41f32802bd6f2e12e02cf43bb
Jens Dahlkvist <Jens.Dahlkvist@sigma.se>
2021-12-04 23:15:05 +0100

    Added bricks with numbers

--- AdventOfCode2021/Day 4 Silver/Program.cs
+++ AdventOfCode2021/Day 4 Silver/Program.cs
@@ -1,26 +1,37 @@
-﻿int[,] binaryArr = null;
-foreach (var line in TextHelper.InputReader.GetInputLines())
+﻿var inputLines = TextHelper.InputReader.GetInputLines();
+
+string[] drawOrder = null;
+
+
+List<int[,]> bricks = new List<int[,]>();
+
+int[,]? brick = null;
+int? brickLine = null;
+foreach (var line in inputLines)
 {
-    if (binaryArr == null) { binaryArr = new int[line.Length, 2]; }
-    for (int i = 0; i < line.Length; i++)
+    if (line.Contains(',')) { drawOrder = line.Split(',', StringSplitOptions.RemoveEmptyEntries); }
+    else if (line.Length > 0)
     {
-        binaryArr[i, Convert.ToInt32(line[i].ToString())] += 1;
+        if (brick == null)
+        {
+            brick = new int[5, 5];
+            brickLine = 0;
+        }
+        var brickLineValues = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        for (int i = 0; i < brickLineValues.Length; i++)
+        {
+            brick[brickLine.Value, i] = Int32.Parse(brickLineValues[i]);
+        }
+
+        if (brickLine == 4) bricks.Add(brick);
+        else brickLine++;
     }
+    else
+    {
+        brick = null;
+        brickLine = null;
+    }
+
 }
 
-string gamma = string.Empty;
-for (int i = 0; i < binaryArr.Length / 2; i++)
-{
-    gamma += binaryArr[i, 0] > binaryArr[i, 1] ? 0 : 1;
-}
-
-string epsilon = string.Empty;
-foreach (var item in gamma)
-{
-    epsilon += item == '1' ? '0' : '1';
-}
-
-var gammaDecimal = Convert.ToInt32(gamma, 2);
-var epsilonDecimal = Convert.ToInt32(epsilon, 2);
-
-Console.WriteLine(gammaDecimal * epsilonDecimal);
+var apa = 0;
