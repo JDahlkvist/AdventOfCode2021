14166c178799c24592d5a2420a93c7c80602ebf2
Jens Dahlkvist <Jens.Dahlkvist@sigma.se>
2021-12-05 09:42:37 +0100

    Temp

--- AdventOfCode2021/Day 4 Silver/Program.cs
+++ AdventOfCode2021/Day 4 Silver/Program.cs
@@ -1,15 +1,70 @@
 ﻿var inputLines = TextHelper.InputReader.GetInputLines();
 
+int? answer = 0;
 string[] drawOrder = null;
-List<int[,]> _boards = new List<int[,]>();
+Dictionary<int, string[,]> _boards = new Dictionary<int, string[,]>();
 
 CreateBoardsAndDrawOrder();
+for (int i = 0; i < drawOrder.Length; i++)
+{
+    for (int j = 0; j < _boards.Keys.Count - 1; j++)
+    {
+        answer = MarkEachBoardAndSearchForBingo(_boards[j], drawOrder[i]).Value;
+        if (answer.HasValue) break;
+    }
+    if (answer.HasValue) break;
+}
+
+Console.WriteLine(answer);
 
 
 //Methods
+int? MarkEachBoardAndSearchForBingo(string[,] board, string number)
+{
+    board.
+
+    bool bingo = false;
+    //Mark number as X if found
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if(board[i,j] == number)
+            {
+                board[i, j] = "X";
+            }
+        }
+    }
+
+    var apa = 2;
+    //Search for 5 X horizontally
+    for (int i = 0; i < board.Length; i++)
+    {
+        string[] row = new string[5];
+        board.CopyTo(row, i);
+
+        if (row.All(row => row == "X"))
+        {
+            bingo = true;
+            break;
+        };
+    }
+    return null;
+}
+
+string[] GetDimRowVals (string [,] multiDimArr,int  rowNumber)
+{
+    string[] rowVals = new string[5];
+    for (int i = 0;i < multiDimArr.Length; i++)
+    {
+        rowVals[i] = multiDimArr[rowNumber, i];
+    }
+    return rowVals;
+}
+
 void CreateBoardsAndDrawOrder()
 {
-    int[,]? board = null;
+    string[,]? board = null;
     int? boardLine = null;
     foreach (var line in inputLines)
     {
@@ -18,16 +73,21 @@ void CreateBoardsAndDrawOrder()
         {
             if (board == null)
             {
-                board = new int[5, 5];
+                board = new string[5, 5];
                 boardLine = 0;
             }
             var brickLineValues = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
             for (int i = 0; i < brickLineValues.Length; i++)
             {
-                board[boardLine.Value, i] = Int32.Parse(brickLineValues[i]);
+                board[boardLine.Value, i] = (brickLineValues[i]);
             }
 
-            if (boardLine == 4) _boards.Add(board);
+            if (boardLine == 4)
+            {
+                var key = !_boards.Keys.Any() ? 0 : _boards.Keys.Max() + 1;
+                _boards.Add(key, board);
+            }
+
             else boardLine++;
         }
         else
